a03c87bd1018409e0c79b8b795cbc376ca0c6730
Thiago Vanzele <thiagovanzele@gmail.com>
2023-03-14 16:38:53 -0300

    Criando função subtrair

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -4,4 +4,8 @@ public class Calculadora {
 	public int somar(int x, int y) {
 		return x + y;
 	}
+	
+	public int subtrair(int x, int y) {
+		return x - y;
+	}
 }
--- src/Programa.java
+++ src/Programa.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
 
 public class Programa {
 
 	public static void main(String[] args) {
 
+		Calculadora c1 = new Calculadora();
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		int resultadoSoma = c1.somar(x, y);
+		int resultadoSubtrai = c1.subtrair(x, y);
+		
+		System.out.println(resultadoSoma);
+		System.out.println(resultadoSubtrai);
+		
+		
+		
+		sc.close();
+		
 	}
 
 }
